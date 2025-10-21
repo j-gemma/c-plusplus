e6962df81a983c160f08d505736a1e7d70b5df1a
Jacob Gemma <jacobg391@gmail.com>
2025-10-21 00:30:44 -0400

    More chapter 16 quiz

--- Sandbox/cppMan.cpp
+++ Sandbox/cppMan.cpp
@@ -13,19 +13,62 @@ const std::string& getWord(size_t i) {
 int initGame() {
 	Session session{};
 
-	std::cout << "Welcome to C++man (a variant of Hangman)\n"
-		"To win : guess the word. To lose : run out of pluses.\n\n";
+	session.displayState();
+	
+	return 0;
+}
 
-	std::cout << "The word is: ";
-	for (size_t i{}; i < size(session.word()); i++) std::cout << "_";
+int Session::replaceChar(size_t i, char c) {
+	m_stateDisplay[i] = c;
+	return 0;
+}
+
+int Session::addToGuessed(char letter) {
+	for (char c : m_guessed) {
+		if (c == letter) {
+			std::cout << "You already guessed that letter.";
+			return 1;
+		}
+	}
+	m_guessed.push_back(letter);
+	return 0;
+}
+
+int Session::displayState() {
+
+	std::cout << std::format("The word is: {}", this -> getDisplayState());
 
 	char letter{ getTFromUser<char>("\nEnter your next letter: ") };
 	while (!isalpha(letter)) {
 		std::cout << "That wasn't valid input. Please try again\n";
 		letter = getTFromUser<char>("Enter your next letter: ");
 	}
-	std::cout << std::format("You entered: {}", letter);
+	this -> addToGuessed(letter);
 
-	
+	std::cout << std::format("You entered: {}", letter);
 	return 0;
+}
+
+std::vector<char> Session::initStateDisplay() {
+	std::vector<char> stateDisplay{};
+	for (size_t i{}; i < size(this->word()); i++) {
+		stateDisplay.push_back('_');
+	}
+	return stateDisplay;
+}
+
+std::vector<char> Session::initUnguessed() {
+	std::vector<char> unguessed{};
+	for (char c : this->word()) {
+		unguessed.push_back(c);
+	}
+	return unguessed;
+}
+
+std::string Session::getDisplayState() {
+	std::string displayState{};
+	for (char c : m_stateDisplay) {
+		displayState += c;
+	}
+	return displayState;
 }
--- Sandbox/cppMan.h
+++ Sandbox/cppMan.h
@@ -13,6 +13,7 @@ namespace WordList {
 class Session {
 private:
 	const std::string& m_word{};
+	std::vector<char> m_unguessed{};
 	std::vector<char> m_stateDisplay{};
 	std::vector<char> m_guessed{};
 	int m_guesses{};
@@ -21,71 +22,25 @@ private:
 public:
 	Session()
 		:m_word{ WordList::words[Random::get<size_t>(0, size(WordList::words) - 1)] }
-		, m_stateDisplay{} {
+		, m_unguessed{ initUnguessed() }
+		,m_stateDisplay{ initStateDisplay() }
+	{
 	}
 	const std::string& word() { return m_word; }
 
-	int initStateDisplay();
+	std::string getDisplayState();
+
+	std::vector<char> initStateDisplay();
+
+	std::vector<char> initUnguessed();
 
 	int replaceChar(size_t i, char c);
 
-	std::string wordWithGuesses();
-
 	int addToGuessed(char letter);
 
-	int displayState(Session& session);
+	int displayState();
 
 };
-
-int Session::initStateDisplay() {
-	for (char c : this->word()) {
-		m_stateDisplay.push_back('_');
-	}
-	return 0;
-}
-
-int Session::replaceChar(size_t i, char c) {
-	m_stateDisplay[i] = c;
-}
-
-std::string Session::wordWithGuesses() {
-	std::string s{ this->word() };
-	for (size_t i{}; i < size(s); i++) {
-		for (char c : m_guessed) {
-			if (c == s[i])	this->replaceChar(i, c);
-		}
-	}
-
-
-	return "";
-}
-
-int Session::addToGuessed(char letter) {
-	for (char c : m_guessed) {
-		if (c == letter) {
-			std::cout << "You already guessed that letter.";
-			return 1;
-		}
-	}
-	m_guessed.push_back(letter);
-	return 0;
-}
-
-int Session::displayState(Session& session) {
-
-	std::cout << std::format("The word is: {}", session.wordWithGuesses());
-	
-
-	char letter{ getTFromUser<char>("\nEnter your next letter: ") };
-	while (!isalpha(letter)) {
-		std::cout << "That wasn't valid input. Please try again\n";
-		letter = getTFromUser<char>("Enter your next letter: ");
-	}
-	session.addToGuessed(letter);
-
-	std::cout << std::format("You entered: {}", letter);
-
-}
 
 int playCPPMan();
 
