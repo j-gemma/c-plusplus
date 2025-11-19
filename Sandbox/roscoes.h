b597d1d3d9d5adbc71544012bb856e59ecbad5b1
j-gemma <jacobg391@gmail.com>
2025-11-19 17:15:21 -0500

    Continue creating roscoes potion emporium for CH17 summary quiz

--- Sandbox/roscoes.h
+++ Sandbox/roscoes.h
@@ -1,12 +1,11 @@
 #pragma once
 #include <array>
 #include <iostream>
+#include <vector>
 
 #include "ClassRandom.h"
 #include "input.h"
 
-static std::string playerName;
-static int wealth{};
 
 namespace Potion {
 
@@ -28,9 +27,39 @@ namespace Potion {
 	static_assert(std::size(cost) == max_potions);
 }
 
-int shop();
+class Player {
+	
+private:
+	const std::string m_name{};
+	std::vector<int> m_inventory{std::vector<int>(Potion::max_potions)};
+	int m_wealth{Random::get(80, 120)};
 
-int greet();
+public:
+
+	Player(const std::string& name)
+		: m_name{ name }
+	{
+	}
+
+	int getWealth(){ return m_wealth; }
+
+	void setWealth(int newTotal) { m_wealth = newTotal; }
+
+	const std::string_view name() { return m_name; }
+
+	void addToInventory(size_t index) { ++m_inventory[index]; }
+	std::vector<int>& getInventory() { return m_inventory; }
+};
+
+int displayShop();
+
+Player greetAndInitPlayer();
+
+bool isValidInput(char input);
+
+int updatePlayer(Player gamer, char purchase);
+
+int printExitStats(Player gamer);
 
 int runRoscoes();
 
